69f1e6fd67180f7582b9620538e1372485c1651f
Sarah Gans <sarah.gans@hotmail.com>
2021-12-19 19:03:10 -0500

    new commit

--- script.js
+++ script.js
@@ -1,15 +1,65 @@
-// Select all »a« elements with a parent class »links« and add a function that is executed on click
-$('a').on('click', function (e) {
+// Scroll function 
+$(document).ready(function () {
+  const scroll = function (scrollTo) {
+    $('html, body').animate({
+      scrollTop: $(scrollTo).offset().top - (nav.outerHeight() - 2)
+    }, 500);
+  }
 
-  // Define variable of the clicked »a« element (»this«) and get its href value.
-  var href = $(this).attr('href');
+  // Scroll for chevron
+  const clickEventStart = () => {
+    $('a.down').on('click', function (e) {
+      e.preventDefault();
+      scroll('main');
+    })
+  }
 
-  // Run a scroll animation to the position of the element which has the same id like the href value.
-  $('html, body').animate({
-    scrollTop: $(href).offset().top
-  }, '300');
+  // Scroll for menu items 
+  const clickEventMenu = () => {
+    $('.nav a').on('click', function (e) {
+    e.preventDefault();
+    scroll($(this).attr('href'))
+    })
+  }
 
-  // Prevent the browser from showing the attribute name of the clicked link in the address bar
-  e.preventDefault();
+  clickEventStart();
+  clickEventMenu();
 
+  // Scrolling for the fixed nav
+  // From https://codepen.io/yuki-san/pen/eJqLNO
+
+  let section = $('header#home, section#about, section#work, section#events, section#contact')
+  let nav = $('nav')
+
+  $(window).on('scroll', function () {
+    const cur_pos = $(this).scrollTop();
+
+    section.each(function () {
+      const top = $(this).offset().top - nav.outerHeight()
+      const bottom = top + $(this).outerHeight();
+
+      if (cur_pos >= top && cur_pos <= bottom) {
+        nav.find('a').removeClass('active');
+        section.removeClass('active');
+
+        $(this).addClass('active');
+        nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+      }
+    });
+  });
+
+  // For hamburger menu
+  $('.hamburger, nav a').on('click', function () {
+    $('.hamburger-menu nav').toggleClass('navVisible')
+    $('.hamburger').toggleClass('is-active')
+  })
+  
+  // For accessible hamburger menu
+  $('.hamburger-menu, nav a').on('keydown', function (e) {
+    if (e.key == 'Enter') {
+      $('.hamburger-menu nav').toggleClass('navVisible')
+      $('.hamburger').toggleClass('is-active')
+    }
+  })
 });
+
